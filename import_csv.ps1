838594b85d00275ab86a58e5e8d83a38d6b68949
sheltowt <sheltowt@gmail.com>
2016-09-08 09:13:57 -0400

    updates

--- import_csv.ps1
+++ import_csv.ps1
@@ -1,8 +1,7 @@
 $Name = @()
 
-Import-Csv /Users/williamshelton/Code/powershell_demo/data/clean.csv |`
+Import-CSV "/Users/williamshelton/Code/powershell_demo/data/clean.csv" |`
     ForEach-Object {
         $Name += $_.Name
+        Write-Host $_.Name
     }
-
-Write-Host $Name
