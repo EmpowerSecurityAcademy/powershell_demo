2b881b867b652f10243501698f4dda534a3b9dbd
sheltowt <sheltowt@gmail.com>
2016-09-07 20:03:38 -0400

    updates

--- scrape_boss.ps1
+++ scrape_boss.ps1
@@ -3,3 +3,11 @@
 $site = Invoke-WebRequest -UseBasicParsing -uri http://www.empoweracademy.io
 
 $site | Out-File /Users/williamshelton/Code/powershell_demo/tmp/empoweracademy.txt
+
+Write-Host $site.Links
+
+Write-Host $site.StatusCode
+
+Foreach ($i in $site.Links) {
+	Write-Host $i
+	}
